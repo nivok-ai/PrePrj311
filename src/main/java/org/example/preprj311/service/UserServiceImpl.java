64e3c2a1bfd3d47ca8baa864ec57290c79865387
nivok-ai <yakovin.shura@mail.ru>
2026-06-02 15:29:28 +0300

    Кодстайл

--- src/main/java/org/example/preprj311/service/UserServiceImpl.java
+++ src/main/java/org/example/preprj311/service/UserServiceImpl.java
@@ -1,6 +1,5 @@
 package org.example.preprj311.service;
 
-//import org.example.preprj311.dao.UserDao;
 import org.example.preprj311.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -12,58 +11,32 @@ import java.util.List;
 @Service
 @Transactional
 public class UserServiceImpl implements UserService {
-    private final UserRepository userDao;
+    private final UserRepository userRepository;
 
     @Autowired
     public UserServiceImpl(UserRepository userDao) {
-        this.userDao = userDao;
+        this.userRepository = userDao;
     }
 
     public void saveUser(User user) {
-        userDao.save(user);
+        userRepository.save(user);
     }
 
     public User getUserById(long id) {
-        return userDao.getReferenceById(id);
+        return userRepository.getReferenceById(id);
     }
 
     public void updateUser(User userUpdated) {
-        userDao.save(userUpdated);
+        userRepository.save(userUpdated);
     }
 
     public void removeUserById(long id) {
-        userDao.deleteById(id);
+        userRepository.deleteById(id);
     }
 
     public List<User> getAllUsers() {
-        return userDao.findAll();
-    }
-    /*private final org.example.preprj311.dao.UserDao userDao;
-
-    @Autowired
-    public UserServiceImpl(UserDao userDao) {
-        this.userDao = userDao;
+        return userRepository.findAll();
     }
 
-    public void saveUser(User user) {
-        userDao.saveUser(user);
-    }
-
-    public User getUserById(long id) {
-        return userDao.getUserById(id);
-    }
-
-    public void updateUser(User userUpdated) {
-        userDao.updateUser(userUpdated);
-    }
-
-    public void removeUserById(long id) {
-        userDao.removeUserById(id);
-    }
-
-    public List<User> getAllUsers() {
-        return userDao.getAllUsers();
-    }
-*/
 }
 
